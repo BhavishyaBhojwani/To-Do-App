1b1ae1cbe8abb76f0854c72d47cbfc138a884d0b
Bhavishya Bhojwani <bhavishyabhojwani1@gmail.com>
2024-07-08 00:17:19 +0530

    splash screen added

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_1/views/splash_view.dart';
 import 'package:get/get.dart';
 import 'views/task_list_view.dart';
 import 'views/add_task_view.dart';
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      home:const AnimatedSplashScreenPage(),
+      
       getPages: [
-        GetPage(name: '/', page: () => TaskListView()),
+        GetPage(name: '/task-list', page: () => TaskListView()), 
         GetPage(name: '/add-task', page: () => AddTaskView()),
         GetPage(name: '/edit-task', page: () => EditTaskView()),
         GetPage(name: '/view-task', page: () => ViewTaskScreen()),
---
+++ lib/views/splash_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter_application_1/views/task_list_view.dart';
+
+class AnimatedSplashScreenPage extends StatelessWidget {
+  const AnimatedSplashScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the screen size
+    final Size size = MediaQuery.of(context).size;
+
+    return AnimatedSplashScreen(
+      duration: 2000, // Duration of animation
+      splash: Image.asset(
+        'assets/images/ic_launcher.png', // Replace with your splash screen image asset
+         width: size.width * 0.9, // Example: 90% of screen width
+        height: size.height * 0.9, 
+        fit: BoxFit.contain, // Cover the entire screen with the image
+      ),
+      nextScreen: TaskListView(),
+      splashTransition: SplashTransition.slideTransition,
+      backgroundColor: Colors.white,
+    );
+  }
+}
